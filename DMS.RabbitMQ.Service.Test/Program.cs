4951593101b35426be165085501cc4c373ca2101
hailang2ll <79522860@qq.com>
2021-07-26 18:41:10 +0800

    MQ服务端与客户端

---
+++ DMS.RabbitMQ.Service.Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using DMS.RabbitMQ.Extensions;
+using Microsoft.Extensions.Hosting;
+
+namespace DMS.RabbitMQ.Service.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateDefaultHost(args).Run();
+        }
+
+        /// <summary>
+        /// 创建
+        /// </summary>
+        /// <param name="configDir"></param>
+        /// <returns></returns>
+        static IHost CreateDefaultHost(string[] args) => new HostBuilder()
+            //.UseLog4net("Config\\log4net.config")
+            .UseRabbitMQ("Config\\rabbitmq.json")
+            .UseBusinessHost()//启用业务主机
+                              //.UseAuditHost()//启用审计队列
+            .Build();
+    }
+}
---
+++ DMS.RabbitMQ/Utils/AttributeUtil.cs
@@ -0,0 +1,20 @@
+﻿using DMS.Common.Extensions;
+using DMS.RabbitMQ.Attributes;
+
+namespace DMS.RabbitMQ.Utils
+{
+    public class AttributeUtil
+    {
+        private static RabbitMQAttribute _rabbitMQAttribute;
+        public static RabbitMQAttribute GetRabbitMQAttribute<T>()
+        {
+            if (_rabbitMQAttribute.IsNullOrEmpty())
+            {
+                var typeOfT = typeof(T);
+                _rabbitMQAttribute = typeOfT.GetCustomAttribute<RabbitMQAttribute>();
+            }
+
+            return _rabbitMQAttribute;
+        }
+    }
+}
